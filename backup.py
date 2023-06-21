bd5dd2d520476dd6ca81d0e551368c1ce65897c6
Chengzhou Wu <71668362+chengzhouwu@users.noreply.github.com>
2023-06-21 17:12:46 -0500

    Update and rename backup to backup.py

---
+++ backup.py
@@ -0,0 +1,26 @@
+# example
+# import libraries
+import pandas as pd
+import matplotlib.pyplot as plt
+
+# Read data CSV file
+data = pd.read_csv('mydata.csv',index_col=0)
+
+# Display the contents of the DataFrame
+# plt.boxplot(data)
+# plt.show()
+#type(data)
+#print(data.shape)
+
+# Read batch and mod1 CSV file
+batch = pd.read_csv('batch.csv')
+mod = pd.read_csv('mod1.csv')
+# Extract batch values as a list
+batch = batch['x'].tolist()
+mod = mod['x'].tolist()
+# Display the batch list
+print(batch)
+type(batch)
+print(mod)
+type(mod)
+#print(data.shape)
